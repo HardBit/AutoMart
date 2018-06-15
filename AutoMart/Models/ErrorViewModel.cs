9c8f12d9146dcbea8d34bac229b087dd89e4981e
Ivan Gonzalez <me@hard-bit.net>
2018-06-14 22:04:32 -0500

    Initial commit

---
+++ AutoMart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AutoMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
